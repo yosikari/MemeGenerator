7663c874615f0b9099005db3b4215a7e403b8f58
yosikari <yosikari@gmail.com>
2022-12-16 14:45:26 +0200

    add save/download features
    add the option to save the meme, the meme will appear in MyMems page.
    The user can download the saved meme, delete it or re-edit it.
    
    the user can also download the meme withoud first saving it

--- js/contraller.js
+++ js/contraller.js
@@ -6,6 +6,8 @@ let gStartPos
 let isDrag = false
 var currImgId = null
 var textBoxIndex = 0
+var numTextBoxes = 1
+
 const TOUCH_EVS = ['touchstart', 'touchmove', 'touchend']
 var currColor = 'white'
 var currFont = 'Impact'
@@ -28,20 +30,22 @@ function onImgClick(id) {
 
 
 function onCloseEditor() {
-    if (confirm('Are you sure?')) {
-        document.querySelector('.editor').style.display = 'none'
-        document.querySelector('.search-bar').style.display = 'flex'
-        document.querySelector('.images-container').style.display = 'grid'
-
-
-        document.querySelector('.text-input').value = ''
-        clearTextBoxes()
-        addTextBox("", 10, 60, gCanvas.width, 60, currColor, currFont)
-        textBoxIndex = 0
-    }
+    document.querySelector('.editor').style.display = 'none'
+    document.querySelector('.search-bar').style.display = 'flex'
+    document.querySelector('.images-container').style.display = 'grid'
+    document.querySelector('.text-input').value = ''
+    clearTextBoxes()
+    addTextBox("", 10, 60, gCanvas.width, 60, currColor, currFont)
+    textBoxIndex = 0
 }
 
 
+
+function onCloseMyMemes() {
+    onCloseEditor()
+    document.querySelector('.my-memes').style.display = 'none'
+    document.querySelector('.search-bar').style.display = 'flex'
+}
 
 
 //Handle the listeners
@@ -55,11 +59,6 @@ function addListeners() {
     })
 }
 
-
-// function renderCanvas() { //todo for touch-ev
-//     gCtx.fillRect(0, 0, gElCanvas.width, gElCanvas.height)
-//     renderCircle()
-// }
 
 function addMouseListeners() {
     gCanvas.addEventListener('mousemove', onMove)
@@ -77,7 +76,12 @@ function onDown(ev) {
     // Get the ev pos from mouse or touch
     const pos = getEvPos(ev)
     //Get the text to move
-    if (!checkIfClickedTextBox(pos)) return
+    if (!checkIfClickedTextBox(pos)) {
+        textBoxIndex = -1
+        renderEditor()
+        return
+    }
+    textBoxIndex = getSelectedTextBoxIndex()
     isDrag = true
 
     //Save the pos we start from
@@ -135,26 +139,32 @@ function resizeCanvas() {
 }
 
 function onMyMemes() {
-    console.log('my-memes')
+    onCloseEditor()
+    document.querySelector('.my-memes').style.display = 'block'
+    document.querySelector('.search-bar').style.display = 'none'
+    document.querySelector('.images-container').style.display = 'none'
+    renderSavedMemes()
 }
 
 function onGallery() {
-    console.log('gallery')
+    onCloseMyMemes()
 }
 
 function onAbout() {
-    console.log('about')
+    alert('coming soon...')
 }
 
 function onSetColor(elColorPicker) {
     currColor = elColorPicker.value
-    setColorInTextBox(textBoxIndex, currColor)
+    let index = textBoxIndex < 0 ? numTextBoxes - 1 : textBoxIndex
+    setColorInTextBox(index, currColor)
     renderEditor()
 }
 
 function onSetFont(elFontSelector) {
     currFont = elFontSelector.value
-    setFontInTextBox(textBoxIndex, currFont)
+    let index = textBoxIndex < 0 ? numTextBoxes - 1 : textBoxIndex
+    setFontInTextBox(index, currFont)
     renderEditor()
 }
 
@@ -162,7 +172,7 @@ function renderImgs() {
     let imgs = getImages()
     let strHtml = ''
     imgs.forEach(img => {
-        strHtml += `<div class="meme-box" onclick="onImgClick(${img.id})"><img class="meme-img" src="img/meme-imgs (square)/${img.url}"></div>`
+        strHtml += `<div class="meme-box" onclick="onImgClick(${img.id})"><img class="meme-img" src="${img.url}"></div>`
     })
     document.querySelector('.images-container').innerHTML = strHtml
 }
@@ -180,10 +190,16 @@ function toggleSideNav() {
 
 
 
-function renderEditor() {
-    let imgs = getImages()
-    let currimg = imgs.find(img => img.id === currImgId)
-    let imgUrl = `img/meme-imgs (square)/${currimg.url}`
+function renderEditor(renderSavedMem = false, id = currImgId) {
+    let imgUrl
+    if (renderSavedMem) { //for mems the id is the index in the mem array
+        imgUrl = getSavedMemes()[id].dataUrl
+    }
+    else { //for images the id is not the index so we need to go over the images
+        let imgs = getImages()
+        let currimg = imgs.find(img => img.id === id)
+        imgUrl = currimg.url
+    }
     make_base(imgUrl);
     let textBoxes = getTextBoxes()
     textBoxes.forEach((textBox, index) => {
@@ -215,15 +231,22 @@ function make_base(imgUrl) {
 function onTextInput() {
     let str = document.querySelector('.text-input').value
     renderEditor()
-    let pos = getTextBoxPos(textBoxIndex)
-    drawText(str, pos.x, pos.y)
+    let index = textBoxIndex < 0 ? numTextBoxes - 1 : textBoxIndex
+    let pos = getTextBoxPos(index)
+    drawText(str, pos.x, pos.y, index)
 }
 
 
 
 function drawText(str, x, y, index = textBoxIndex, color = currColor, font = currFont) {
-    console.log(currFont)
     const ctx = gCanvas.getContext("2d")
+    if (index === textBoxIndex) {
+        ctx.beginPath()
+        ctx.rect(0, y - 53, gCanvas.width, 60)
+        ctx.lineWidth = 1;
+        ctx.stroke()
+        document.querySelector('.text-input').value = str
+    }
     ctx.font = `60px ${font}`
     ctx.fillStyle = color
     ctx.lineWidth = 5
@@ -234,13 +257,66 @@ function drawText(str, x, y, index = textBoxIndex, color = currColor, font = cur
 
 function onAddNewTextBox() {
     document.querySelector('.text-input').value = ''
-    textBoxIndex++
-    if (textBoxIndex === 1) {
+    numTextBoxes++
+    if (numTextBoxes === 2) {
         addTextBox('', 10, gCanvas.height - 100, gCanvas.width, 60, currColor, currFont)
     } else {
         addTextBox('', 10, gCanvas.height / 2, gCanvas.width, 60, currColor, currFont)
     }
-    setTextInTextBox(textBoxIndex, null)
+    setTextInTextBox(numTextBoxes - 1, null)
+    textBoxIndex = numTextBoxes - 1
+}
 
 
+function onSaveMeme() {
+    let dataUrl = gCanvas.toDataURL("image/jpeg")
+    addMemeToMyMemes(dataUrl)
+    //open "saved" auto-closed modal
+    document.querySelector('.saved-modal').style.display = 'block'
+    setTimeout(() => { document.querySelector('.saved-modal').style.display = 'none' }, 3000)
+}
+
+function renderSavedMemes() {
+    let strHtml = ''
+    getSavedMemes().forEach((currMem, index) =>
+        strHtml += `<div class="saved-mem" onclick="onSavedMemClick(${index})" style="background-image: 
+        url(${currMem.dataUrl}); background-size: cover;">
+        <img class="editor-btns btn-download btn-my-memes" onclick="onDownloadSavedMeme(${index})" 
+        src="img/btns/download.png"><img class="editor-btns top btn-remove btn-my-memes"
+        onclick="onRemoveSavedMeme(${index})" src="img/btns/remove.png"></div>`)
+    document.querySelector('.my-memes-container').innerHTML = strHtml
+}
+
+
+function onDownloadSavedMeme(index) {
+    event.stopPropagation()
+    let dataUrl = getSavedMemes()[index].dataUrl
+    downloadURL(dataUrl)
+
+}
+
+function onDownloadCanvasMeme() {
+    let dataUrl = gCanvas.toDataURL("image/jpeg")
+    downloadURL(dataUrl)
+}
+
+function downloadURL(url) {
+    var link = document.createElement("a");
+    link.href = url;
+    link.download = "myMem";
+    link.click();
+}
+
+function onRemoveSavedMeme(index) {
+    event.stopPropagation()
+    removeSavedMeme(index)
+    renderSavedMemes()
+}
+
+function onSavedMemClick(index){
+    renderEditor(true, index)
+    document.querySelector('.editor').style.display = 'grid'
+    document.querySelector('.search-bar').style.display = 'none'
+    document.querySelector('.images-container').style.display = 'none'
+    document.querySelector('.my-memes').style.display = 'none'
 }
--- js/service.js
+++ js/service.js
@@ -1,74 +1,108 @@
 'use strict'
 
+const IMGS_KEY = 'images'
+const MEMES_KEY = 'memes'
+
 var gImages = [
-    {id: getRandomId(), url: '1.jpg', keywords: ['funny', 'cat']},
-    {id: getRandomId(), url: '2.jpg', keywords: ['funny', 'cat']},
-    {id: getRandomId(), url: '3.jpg', keywords: ['funny', 'cat']},
-    {id: getRandomId(), url: '4.jpg', keywords: ['funny', 'cat']},
-    {id: getRandomId(), url: '5.jpg', keywords: ['funny', 'cat']},
-    {id: getRandomId(), url: '6.jpg', keywords: ['funny', 'cat']},
-    {id: getRandomId(), url: '7.jpg', keywords: ['funny', 'cat']},
-    {id: getRandomId(), url: '8.jpg', keywords: ['funny', 'cat']},
-    {id: getRandomId(), url: '9.jpg', keywords: ['funny', 'cat']},
-    {id: getRandomId(), url: '10.jpg', keywords: ['funny', 'cat']},
-    {id: getRandomId(), url: '11.jpg', keywords: ['funny', 'cat']},
-    {id: getRandomId(), url: '12.jpg', keywords: ['funny', 'cat']},
-    {id: getRandomId(), url: '13.jpg', keywords: ['funny', 'cat']},
-    {id: getRandomId(), url: '14.jpg', keywords: ['funny', 'cat']},
-    {id: getRandomId(), url: '15.jpg', keywords: ['funny', 'cat']},
-    {id: getRandomId(), url: '16.jpg', keywords: ['funny', 'cat']},
-    {id: getRandomId(), url: '17.jpg', keywords: ['funny', 'cat']},
-    {id: getRandomId(), url: '18.jpg', keywords: ['funny', 'cat']},
+    { id: getRandomId(), url: 'img/meme-imgs (square)/1.jpg', keywords: ['funny', 'cat'] },
+    { id: getRandomId(), url: 'img/meme-imgs (square)/2.jpg', keywords: ['funny', 'cat'] },
+    { id: getRandomId(), url: 'img/meme-imgs (square)/3.jpg', keywords: ['funny', 'cat'] },
+    { id: getRandomId(), url: 'img/meme-imgs (square)/4.jpg', keywords: ['funny', 'cat'] },
+    { id: getRandomId(), url: 'img/meme-imgs (square)/5.jpg', keywords: ['funny', 'cat'] },
+    { id: getRandomId(), url: 'img/meme-imgs (square)/6.jpg', keywords: ['funny', 'cat'] },
+    { id: getRandomId(), url: 'img/meme-imgs (square)/7.jpg', keywords: ['funny', 'cat'] },
+    { id: getRandomId(), url: 'img/meme-imgs (square)/8.jpg', keywords: ['funny', 'cat'] },
+    { id: getRandomId(), url: 'img/meme-imgs (square)/9.jpg', keywords: ['funny', 'cat'] },
+    { id: getRandomId(), url: 'img/meme-imgs (square)/10.jpg', keywords: ['funny', 'cat'] },
+    { id: getRandomId(), url: 'img/meme-imgs (square)/11.jpg', keywords: ['funny', 'cat'] },
+    { id: getRandomId(), url: 'img/meme-imgs (square)/12.jpg', keywords: ['funny', 'cat'] },
+    { id: getRandomId(), url: 'img/meme-imgs (square)/13.jpg', keywords: ['funny', 'cat'] },
+    { id: getRandomId(), url: 'img/meme-imgs (square)/14.jpg', keywords: ['funny', 'cat'] },
+    { id: getRandomId(), url: 'img/meme-imgs (square)/15.jpg', keywords: ['funny', 'cat'] },
+    { id: getRandomId(), url: 'img/meme-imgs (square)/16.jpg', keywords: ['funny', 'cat'] },
+    { id: getRandomId(), url: 'img/meme-imgs (square)/17.jpg', keywords: ['funny', 'cat'] },
+    { id: getRandomId(), url: 'img/meme-imgs (square)/18.jpg', keywords: ['funny', 'cat'] },
 ];
 
 var gTextBoxes = []
-let gTextBoxToMove = null
+let gIndexSelctedTextBox = -1
+
+var gSavedMemes = []
 
 
-function getImages(){
+function getImages() {
     return gImages
 }
-function getTextBoxes(){
-   return gTextBoxes
+function getTextBoxes() {
+    return gTextBoxes
 }
 
-function getTextBoxPos(index){
-    return {x:gTextBoxes[index].x , y :gTextBoxes[index].y}
+function getTextBoxPos(index) {
+    return { x: gTextBoxes[index].x, y: gTextBoxes[index].y }
 }
 
-function setTextInTextBox(index,text){
-     gTextBoxes[index].text = text
+function setTextInTextBox(index, text) {
+    gTextBoxes[index].text = text
 }
 
-function setColorInTextBox(index,color){
+function setColorInTextBox(index, color) {
     gTextBoxes[index].color = color
 }
 
-function setFontInTextBox(index,font){
+function setFontInTextBox(index, font) {
     gTextBoxes[index].font = font
 }
 
-function addTextBox(text ,x, y, width, height, color, font){
-    gTextBoxes.push({text:text, x :x, y: y ,width: width, height: height, color: color, font: font})
+function addTextBox(text, x, y, width, height, color, font) {
+    gTextBoxes.push({ text: text, x: x, y: y, width: width, height: height, color: color, font: font })
 }
 
-function checkIfClickedTextBox(clickedPos){
-    for(var i = 0 ; i < gTextBoxes.length; i++){
-        if(gTextBoxes[i].y - gTextBoxes[i].height <= clickedPos.y &&
-           gTextBoxes[i].y > clickedPos.y){
-             gTextBoxToMove =  gTextBoxes[i]
-             return true
+function checkIfClickedTextBox(clickedPos) {
+    for (var i = 0; i < gTextBoxes.length; i++) {
+        if (gTextBoxes[i].y - gTextBoxes[i].height <= clickedPos.y &&
+            gTextBoxes[i].y > clickedPos.y) {
+            gIndexSelctedTextBox = i
+            return true
         }
     }
-  return false
+    return false
 }
 
 function moveTextBox(dx, dy) {
-    gTextBoxToMove.x += dx
-    gTextBoxToMove.y += dy
+    gTextBoxes[gIndexSelctedTextBox].x += dx
+    gTextBoxes[gIndexSelctedTextBox].y += dy
 
 }
 
-function clearTextBoxes(){
+function getSelectedTextBoxIndex() {
+    return gIndexSelctedTextBox;
+}
+
+function clearTextBoxes() {
     gTextBoxes = []
+    gIndexSelctedTextBox = -1
+}
+
+
+function addMemeToMyMemes(dataUrl) {
+    gSavedMemes.push({dataUrl:dataUrl})
+    _SaveMemesToStorage()
+}
+function removeSavedMeme(index){
+    gSavedMemes.splice(index,1)
+    _SaveMemesToStorage()
+
+}
+
+function getSavedMemes() {
+    gSavedMemes = loadFromStorage(MEMES_KEY) 
+    return gSavedMemes
+}
+
+function _SaveImgsToStorage() {
+    saveToStorage(IMGS_KEY, 'images')
+}
+
+function _SaveMemesToStorage() {
+    saveToStorage(MEMES_KEY, gSavedMemes)
 }
